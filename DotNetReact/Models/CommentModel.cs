7897ba337b53bcfd571bb087770268eaadd2e0e2
Bao Tran <tranthebaocntt31b@gmail.com>
2020-03-21 13:44:40 +0700

    Tutorial react

---
+++ DotNetReact/Models/CommentModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ReactDemo.Models {
+	public class CommentModel {
+		public int Id { get; set; }
+		public string Author { get; set; }
+		public string Text { get; set; }
+		public string ABC { get; set;}
+	}
+}
